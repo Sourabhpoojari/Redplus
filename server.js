288de6d989b5635f03fccb100cfe5faa2fbe6017
praneethk186 <praneethk186@gmail.com>
2021-03-19 11:27:29 +0530

    FIRST COMMIT

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ connectDB();
 // app.use()
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
+
 // routes
 app.use('/api/user/',require('./routes/user/user-routes'));
+app.use('/api/bloodbank/',require('./routes/bloodbank/bloodbank-routes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`)); 
