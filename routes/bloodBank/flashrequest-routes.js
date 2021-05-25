25a9e32957ac35e26a3cba7b9b55754a44721ae1
Sourabhpoojari <sourabhpoojari674@gmail.com>
2021-05-25 14:06:21 +0530

    heroku  server 1

--- routes/bloodBank/flashrequest-routes.js
+++ routes/bloodBank/flashrequest-routes.js
@@ -1,9 +1,14 @@
 const router = require('express').Router(),
-{check} = require('express-validator'),
-FlashRequest = require('../../controllers/bloodbank-Controllers/flashRequest-controller'),
-auth = require('../../middleware/bloodBankAuth');
+	{ check } = require('express-validator'),
+	FlashRequest = require('../../controllers/bloodBank-Controllers/flashRequest-controller'),
+	auth = require('../../middleware/bloodBankAuth');
 
-router.post('/',auth,check('lat','Please Give Latitude value').exists(),
-check('lang','Please Give Langitude value ').exists(),FlashRequest.flashRequest);
+router.post(
+	'/',
+	auth,
+	check('lat', 'Please Give Latitude value').exists(),
+	check('lang', 'Please Give Langitude value ').exists(),
+	FlashRequest.flashRequest
+);
 
 module.exports = router;
