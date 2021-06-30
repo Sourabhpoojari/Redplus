4fd34bf035a95c64aeddc493347695395d602cff
Sourabhpoojari <sourabhpoojari674@gmail.com>
2021-06-30 14:08:17 +0530

    camp schedule request model

--- models/admin/requests/campsheduleReuestSchema.js
+++ models/admin/requests/campsheduleReuestSchema.js
@@ -1,56 +1,67 @@
 const mongoose = require('mongoose');
 
 const campsheduleRequestSchema = mongoose.Schema({
-    donor:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
-    },
-    bloodbank:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'BloodBank'
-    },
-    campAddress:{
-        type:String,
-        required:true
-    },
-    campName:{
-        type:String,
-        required:true
-    },
-    campSchedule:{
-        type:Date,
-    },
-    capacity:{
-        type:Number,
-    },
-    community:{
-        type:String,
-    },
-    referenceId:{
-        type:String,
-    },
-    sponserOrganization:{
-        type:String,
-    },
-    poster:{
-      type:String
-    },
-    location: {
-      type: {
-        type: "String",
-        default:"Point" // Don't do `{ location: { type: String } }`
-        // 'location.type' must be 'Point'
-        // required: true
-      },
-      // type:Point,
-      coordinates: {
-        type: [Number],
-        required: true
-      }
-    }
-    
-    
+	orgainizer: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+	},
+	bloodBankOrganizer: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'BloodBank',
+	},
+	hospitalOrganizer: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Hospital',
+	},
+	address: {
+		type: String,
+		required: true,
+	},
+	title: {
+		type: String,
+		required: true,
+	},
+	date: {
+		type: String,
+	},
+	time: {
+		type: String,
+	},
+	donations: {
+		type: Number,
+	},
+	organization: {
+		type: String,
+	},
+	requestForm: {
+		type: String,
+	},
+	poster: {
+		type: String,
+	},
+	bloodBanks: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'BloodBank',
+		},
+	],
+	location: {
+		type: {
+			type: 'String',
+			default: 'Point', // Don't do `{ location: { type: String } }`
+			// 'location.type' must be 'Point'
+			// required: true
+		},
+		// type:Point,
+		coordinates: {
+			type: [Number],
+			required: true,
+		},
+	},
 });
-campsheduleRequestSchema.index({ location: "2dsphere" });
+campsheduleRequestSchema.index({ location: '2dsphere' });
 
-module.exports  = mongoose.models.OrganizeCampRequest || mongoose.model('OrganizeCampRequest',campsheduleRequestSchema);
+module.exports = mongoose.model(
+	'OrganizeCampRequest',
+	campsheduleRequestSchema
+);
