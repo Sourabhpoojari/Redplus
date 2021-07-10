bf6bb86cb8feea4c6578de0694821dc607f8e367
Sourabhpoojari <sourabhpoojari674@gmail.com>
2021-07-10 21:45:15 +0530

    get camps

--- routes/user/organizeCamp-routes.js
+++ routes/user/organizeCamp-routes.js
@@ -6,22 +6,23 @@ const router = require('express').Router(),
 router.post(
 	'/',
 	auth,
-	check('address','Camp Address is required!').not(),
-	check('title','Camp title is required!').not(),
-	check('date','Shedule date is required!').not(),
-    check('timefrom','time from is required!').not(),
-    check('timeto','time to is required!').not(),
-	check('donations','Enter a valid capacity').not(),
-	check('community','Enter a valid community').not(),
-	check('organization','Enter a reference id').not(),
-	check('poster','choose valid poster').not(),
-	check('requestForm','choice request form').not(),
-    check('bloodBanks','please select bloodbanks').not(),
-	check('campLat','Lattitude value is required').not(),
-	check('campLng','Longitude value is required!').not(),
+	check('address', 'Camp Address is required!').not(),
+	check('title', 'Camp title is required!').not(),
+	check('date', 'Shedule date is required!').not(),
+	check('timefrom', 'time from is required!').not(),
+	check('timeto', 'time to is required!').not(),
+	check('donations', 'Enter a valid capacity').not(),
+	check('community', 'Enter a valid community').not(),
+	check('organization', 'Enter a reference id').not(),
+	check('poster', 'choose valid poster').not(),
+	check('requestForm', 'choice request form').not(),
+	check('bloodBanks', 'please select bloodbanks').not(),
+	check('campLat', 'Lattitude value is required').not(),
+	check('campLng', 'Longitude value is required!').not(),
 
 	campOrganizeController.campRequest
 );
+router.get('/', auth, campOrganizeController.getCamps);
 
 router.get('/bloodBanks', auth, campOrganizeController.getBloodBanks);
 
