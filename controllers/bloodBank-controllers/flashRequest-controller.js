a8f3b7d2eba9491302c8e7e79a3b53b353294ec7
praneethk186 <praneethk186@gmail.com>
2021-07-13 17:07:51 +0530

    chnags

--- controllers/bloodBank-controllers/flashRequest-controller.js
+++ controllers/bloodBank-controllers/flashRequest-controller.js
@@ -13,7 +13,7 @@ client = require('twilio')(accountSid, authToken);
 const fast2sms = require('fast-two-sms');
  const options = {
               authorization : 'cPziVu4sAdfIwCq3GnpvEJ5X1O8WQgtZrSa2KBRTUol97myHYbuAhEte2rM5oRcHwOFfIi08Lv9p6Sbq' , 
-              message : "Red Plus",  
+              message : "Red Plus varification",  
               numbers :[+919741925186]
               }
  
